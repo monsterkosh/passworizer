0dd19fd7a3a9684c7526a34350705dc808d37add
Emiliano Kosh <mnnnkosh@gmail.com>
2022-02-21 14:48:15 -0300

    special case diff 0

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function Passworizer(difficulty = 1) {
-  // Difficulty 1: 8 Characters (4 Lowercase, 1 Uppercase, 2 Numbers, 1 Symbol)
-  // Difficulty 2: 12 Characters (4 Lowercase, 4 Uppercase, 2 Numbers, 2 Symbol)
-  // Difficulty 3: 18 Characters (5 Lowercase, 5 Uppercase, 4 Numbers, 4 Symbol)
+function Passworizer(difficulty) {
+  // Generated password will always start with a Capital letter.
 
+  // Maximum difficulty
   if (difficulty > 3) throw new Error('Maximun level of compression is: 3');
 
+  // Characters
   const characters = {
     lowChars: 'abcdefghijklmnopqrstuvwxyz', // 26
     upperChars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', // 26
@@ -12,14 +12,17 @@ function Passworizer(difficulty = 1) {
     symbols: '@#$%&-_=+()[]{}<>.!?"', //21
   };
 
+  // Gets 1 random caracter from a characters type
   const getOneRandomChar = (string) => {
     if (typeof string !== 'string') throw new Error('Error: String type');
     const array = string.split('');
     const lenghtOfarray = array.length;
     const randomNumber = Math.floor(Math.random() * lenghtOfarray);
+    // returns a string (1 char)
     return array[randomNumber];
   };
 
+  // Gets various characters from a characters type
   const getVariousRandomChars = (times, type) => {
     let result = [];
     if (typeof times !== 'number')
@@ -28,14 +31,19 @@ function Passworizer(difficulty = 1) {
       const element = getOneRandomChar(type);
       result.push(element);
     }
+    // returns an array of X(times) character/s(type)
     return result;
   };
 
-  const shuffledArray = (array) => {
+  // Shuffles an array, returns array
+  const shuffleArray = (array) => {
     return array.sort((a, b) => 0.5 - Math.random());
   };
 
+  // Generates the final password
+  // parameters should be (numbers) the amount of chars desired for each type
   const generatePassword = (lows, uppers, numbers, symbols) => {
+    // Get the individual results of each type
     const lowCharsResult = getVariousRandomChars(lows, characters.lowChars);
     const firstUpperCharsResult = getVariousRandomChars(
       1,
@@ -47,18 +55,32 @@ function Passworizer(difficulty = 1) {
     );
     const numbersResult = getVariousRandomChars(numbers, characters.numbers);
     const symbolsResult = getVariousRandomChars(symbols, characters.symbols);
+
+    // Concat a temporary result without the First Upper Character
     const temporaryResult = lowCharsResult.concat(
       numbersResult,
       symbolsResult,
       upperCharsResult
     );
-    const shuffledResult = shuffledArray(temporaryResult);
+
+    // Shuffle the temporary result
+    const shuffledResult = shuffleArray(temporaryResult);
+
+    // Join the First Upper Character with the rest of the result already shuffled
     const joinResult = firstUpperCharsResult.concat(shuffledResult);
+
+    // Convert everything to a string
     const stringedResult = joinResult.join('');
     return stringedResult;
   };
 
   switch (difficulty) {
+    // Difficulty 0: 8 Characters (4 Lowercase, 2 Uppercase, 2 Numbers, 0 Symbol)
+    // Difficulty 1 (default): 8 Characters (4 Lowercase, 1 Uppercase, 2 Numbers, 1 Symbol)
+    // Difficulty 2: 12 Characters (4 Lowercase, 4 Uppercase, 2 Numbers, 2 Symbol)
+    // Difficulty 3: 18 Characters (5 Lowercase, 5 Uppercase, 4 Numbers, 4 Symbol)
+    case 0:
+      return generatePassword(4, 2, 2, 0);
     case 1:
       return generatePassword(4, 1, 2, 1);
     case 2:
