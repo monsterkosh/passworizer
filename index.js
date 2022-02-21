a9f9c10d796035a5bc3cfd26892720398b9e7c30
Emiliano Kosh <mnnnkosh@gmail.com>
2022-02-21 14:10:40 -0300

    [all 3 levels] with switch statement

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function Passworizer(difficulty = 1) {
   // Difficulty 1: 8 Characters (4 Lowercase, 1 Uppercase, 2 Numbers, 1 Symbol)
-  // Difficulty 1: 12 Characters (4 Lowercase, 1 Uppercase, 2 Numbers, 1 Symbol)
+  // Difficulty 2: 12 Characters (4 Lowercase, 4 Uppercase, 2 Numbers, 2 Symbol)
+  // Difficulty 3: 18 Characters (5 Lowercase, 5 Uppercase, 4 Numbers, 4 Symbol)
 
-  if (difficulty > 3) return 'Maximun level of compression is: 3';
+  if (difficulty > 3) throw new Error('Maximun level of compression is: 3');
 
   const characters = {
     lowChars: 'abcdefghijklmnopqrstuvwxyz', // 26
@@ -46,8 +47,56 @@ function Passworizer(difficulty = 1) {
     return stringedResult;
   };
 
-  const result = generateSimplePassword();
-  return result;
+  const generateMediumPassword = () => {
+    const lowCharsResult = getVariousRandomChars(4, characters.lowChars);
+    const firstUpperCharsResult = getVariousRandomChars(
+      1,
+      characters.upperChars
+    );
+    const upperCharsResult = getVariousRandomChars(3, characters.upperChars);
+    const numbersResult = getVariousRandomChars(2, characters.numbers);
+    const symbolsResult = getVariousRandomChars(2, characters.symbols);
+    const temporaryResult = lowCharsResult.concat(
+      numbersResult,
+      symbolsResult,
+      upperCharsResult
+    );
+    const shuffledResult = shuffledArray(temporaryResult);
+    const joinResult = firstUpperCharsResult.concat(shuffledResult);
+    const stringedResult = joinResult.join('');
+    return stringedResult;
+  };
+
+  const generateHardPassword = () => {
+    const lowCharsResult = getVariousRandomChars(5, characters.lowChars);
+    const firstUpperCharsResult = getVariousRandomChars(
+      1,
+      characters.upperChars
+    );
+    const upperCharsResult = getVariousRandomChars(5, characters.upperChars);
+    const numbersResult = getVariousRandomChars(4, characters.numbers);
+    const symbolsResult = getVariousRandomChars(4, characters.symbols);
+    const temporaryResult = lowCharsResult.concat(
+      numbersResult,
+      symbolsResult,
+      upperCharsResult
+    );
+    const shuffledResult = shuffledArray(temporaryResult);
+    const joinResult = firstUpperCharsResult.concat(shuffledResult);
+    const stringedResult = joinResult.join('');
+    return stringedResult;
+  };
+
+  switch (difficulty) {
+    case 1:
+      return generateSimplePassword();
+    case 2:
+      return generateMediumPassword();
+    case 3:
+      return generateHardPassword();
+    default:
+      return generateSimplePassword;
+  }
 }
 
 module.exports = Passworizer;
